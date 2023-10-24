5d092e9384f2374aee9f682dd1de8845528623ca
Noah <100672306+eacxyz@users.noreply.github.com>
2023-10-24 19:20:11 -0400

    Implement basic math operators

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(...args) {
+	let sum = 0;
+	for (let arg of args) sum += arg;
+	return sum;
+}
+
+function subtract() {
+	let diff = arguments[0];
+	for (let i = 1; i < arguments.length; i++) {
+		diff -= arguments[i];
+	}
+	return diff;
+}
+
+function multiply(...args) {
+	let prod = 1;
+	for (let arg of args) prod *= arg;
+	return prod;
+}
+
+function divide() {
+	let quotient = arguments[0];
+	for (let i = 1; i < arguments.length; i++) {
+		quotient /= arguments[i];
+	}
+	return quotient;
+}

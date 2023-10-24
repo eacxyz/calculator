13daad8e0d293b68862f865ad04309577ad4fd06
Noah <100672306+eacxyz@users.noreply.github.com>
2023-10-24 19:32:46 -0400

    Create operate function and edit basic operations
    Changed four basic operations to only take in two numbers because
    operate function only takes in two values.

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-function add(...args) {
-	let sum = 0;
-	for (let arg of args) sum += arg;
-	return sum;
-}
+// function add(...args) {
+// 	let sum = 0;
+// 	for (let arg of args) sum += arg;
+// 	return sum;
+// }
 
-function subtract() {
-	let diff = arguments[0];
-	for (let i = 1; i < arguments.length; i++) {
-		diff -= arguments[i];
-	}
-	return diff;
-}
+// function subtract() {
+// 	let diff = arguments[0];
+// 	for (let i = 1; i < arguments.length; i++) {
+// 		diff -= arguments[i];
+// 	}
+// 	return diff;
+// }
 
-function multiply(...args) {
-	let prod = 1;
-	for (let arg of args) prod *= arg;
-	return prod;
-}
+// function multiply(...args) {
+// 	let prod = 1;
+// 	for (let arg of args) prod *= arg;
+// 	return prod;
+// }
 
-function divide() {
-	let quotient = arguments[0];
-	for (let i = 1; i < arguments.length; i++) {
-		quotient /= arguments[i];
-	}
-	return quotient;
-}
+// function divide() {
+// 	let quotient = arguments[0];
+// 	for (let i = 1; i < arguments.length; i++) {
+// 		quotient /= arguments[i];
+// 	}
+// 	return quotient;
+// }
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 let firstNum;
 let operator;
 let secondNum;
+
+function operate(operator, firstNum, secondNum) {
+	return operator(firstNum, secondNum);
+}
